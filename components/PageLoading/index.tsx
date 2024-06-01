9797972e30919fcf14447a004b02279d18f8c29b
wilderzorn <wilderzorn@163.com>
2024-06-01 18:37:00 +0800

    feat: 添加加载页面

---
+++ components/PageLoading/index.tsx
@@ -0,0 +1,16 @@
+import { Spin } from 'antd'
+const PageLoading = () => {
+  return (
+    <div
+      style={{
+        paddingTop: 100,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center'
+      }}
+    >
+      <Spin />
+    </div>
+  )
+}
+export default PageLoading
